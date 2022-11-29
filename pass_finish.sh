4bc4528b5289dca9af10f260dfa49de72bbc4014
Dimitrios Stamatios Bouras <75038393+jimbou@users.noreply.github.com>
2022-11-29 13:17:08 +0200

    Update pass_finish.sh

--- pass_finish.sh
+++ pass_finish.sh
@@ -5,7 +5,7 @@
 clang -S -emit-llvm $1.c
 llvm-as $1.ll
 
-opt -load build/skeleton/libSkeletonPass.* -skeleton < $1.bc > $1_inst.bc  -enable-new-pm=0  2> code_ll_$1.txt 
+opt -load build/skeleton/libSkeletonPass.* -skeleton < $1.bc > $1_inst.bc   2> code_ll_$1.txt #-enable-new-pm=0  add this for your pc but not for server
 llc -filetype=obj  $1_inst.bc 
 llvm-dis -o $1-debug.ll $1_inst.bc 
 clang  -o $1 $1_inst.o  -O0  
