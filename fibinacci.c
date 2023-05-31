1b754d64da3f39111ccc1f82bd67a5848334e78b
jimbou <el17072@mail.ntua.gr>
2023-05-31 12:44:22 +0300

    Update fibinacci.c

--- fibinacci.c
+++ fibinacci.c
@@ -24,7 +24,7 @@ int main() {
   scanf("%d", &n);
 
   // print the first two terms t1 and t2
-  printf("Fibonacci Series: %d, %d, ", t1, t2);
+  printf("Fibonaccci Series: %d, %d, ", t1, t2);
 
   // print 3rd to nth terms
   for (i = 3; i <= n; ++i) {
